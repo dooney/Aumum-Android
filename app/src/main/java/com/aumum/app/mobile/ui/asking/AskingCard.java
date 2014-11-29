87576615378b046c8e0a77ab42bad335b5669e67
Simpson Du <isimpsondu@gmail.com>
2014-11-30 00:10:53 +1100

    Refactor asking card UI.

--- app/src/main/java/com/aumum/app/mobile/ui/asking/AskingCard.java
+++ app/src/main/java/com/aumum/app/mobile/ui/asking/AskingCard.java
@@ -5,27 +5,23 @@ import android.widget.TextView;
 
 import com.aumum.app.mobile.R;
 import com.aumum.app.mobile.core.model.Asking;
-import com.aumum.app.mobile.ui.view.AvatarImageView;
 
 /**
  * Created by Administrator on 27/11/2014.
  */
 public class AskingCard {
 
-    private AvatarImageView avatarImage;
     private TextView userNameText;
     private TextView createdAtText;
     private TextView questionText;
 
     public AskingCard(View view) {
-        this.avatarImage = (AvatarImageView) view.findViewById(R.id.image_avatar);
         this.userNameText = (TextView) view.findViewById(R.id.text_user_name);
         this.createdAtText = (TextView) view.findViewById(R.id.text_createdAt);
         this.questionText = (TextView) view.findViewById(R.id.text_question);
     }
 
     public void refresh(Asking asking) {
-        avatarImage.getFromUrl(asking.getUser().getAvatarUrl());
         userNameText.setText(asking.getUser().getScreenName());
         createdAtText.setText(asking.getCreatedAtFormatted());
         questionText.setText(asking.getQuestion());
