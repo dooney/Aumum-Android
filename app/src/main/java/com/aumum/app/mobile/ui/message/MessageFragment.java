3b50904ea3fbd8e28221d5846c242301c1638a7e
Simpson Du <isimpsondu@gmail.com>
2015-05-16 09:07:40 +1000

    Refactor layout.

--- app/src/main/java/com/aumum/app/mobile/ui/message/MessageFragment.java
+++ app/src/main/java/com/aumum/app/mobile/ui/message/MessageFragment.java
@@ -1,9 +1,37 @@
 package com.aumum.app.mobile.ui.message;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aumum.app.mobile.R;
+import com.aumum.app.mobile.ui.contact.ContactRequestsActivity;
 
 /**
  * Created by Administrator on 14/05/2015.
  */
 public class MessageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_message, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View contactRequestsLayout = view.findViewById(R.id.layout_contact_requests);
+        contactRequestsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(getActivity(), ContactRequestsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
